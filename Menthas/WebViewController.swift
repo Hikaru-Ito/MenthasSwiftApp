170a40194a806c5dadb04b560e0cdd5b8dc4b851
Hikaru <hikaru.ito.1995@gmail.com>
2016-05-11 20:11:32 +0900

    WebView ProgressAnimation

--- Menthas/WebViewController.swift
+++ Menthas/WebViewController.swift
@@ -7,22 +7,38 @@
 //
 
 import UIKit
+import BusyNavigationBar
 
-class WebViewController: UIViewController {
+class WebViewController: UIViewController, UIWebViewDelegate {
 
     @IBOutlet weak var webView: UIWebView!
 
     var url: String!
     var titie: String!
+    var loadingBarOption = BusyNavigationBarOptions()
     
     override func viewDidLoad() {
         self.titie = title
         let targetURL = NSURL(string: url)
         let req = NSURLRequest(URL: targetURL!)
         webView.loadRequest(req)
+        webView.delegate = self
     }
     
     deinit {
         webView.stopLoading()
+        webView.delegate = nil
+    }
+    
+    // Loading BusyNavBar
+    func webViewDidStartLoad(webView: UIWebView) {
+        loadingBarOption.animationType = .Stripes
+        self.navigationController?.navigationBar.start(loadingBarOption)
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        if !webView.loading {
+            self.navigationController?.navigationBar.stop()
+        }
     }
 }
